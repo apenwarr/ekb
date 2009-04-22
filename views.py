77b204b538340e9f737da0ec809475385a69441c
Avery Pennarun <apenwarr@gmail.com>
2009-04-22 18:58:07 -0400

    Really basic kb article browser, if you go to url /kb/###.
    Where ### is the auto-assigned primary key, which is kind of stupid since it
    changes every time we do "make load".

--- views.py
+++ views.py
@@ -1,18 +1,34 @@
 from django.shortcuts import render_to_response
-import re
+import re, datetime
 from helpers import *
 from kb.models import Doc, Tag
 
-dict = {'menuitems': [('/index/', 'Home'),
+_dict = {'menuitems': [('/index/', 'Home'),
 		      ('/kb/', 'Knowledgebase'),
 		      ],
 	};
 
-def show(req, search=''):
-    search = req.REQUEST.get('q', search)
-    dict['page'] = '/kb/'
+def _render_search(dict, req, search):
     dict['title'] = 'Search: "%s"' % search
     dict['when'] = '5 days ago'
     dict['tags'] = ['this','that','the other thing']
     dict['text'] = "Hey, I'm some text!"
+    return render_to_response('kb/view.html', dict)
+
+def show(req, search=''):
+    search = req.REQUEST.get('q', search)
+
+    dict = {}
+    dict.update(_dict)
+    dict['page'] = '/kb/'
+
+    try:
+	doc = Doc.objects.get(id=atoi(search))
+    except KeyError:
+	return _render_search(dict, req, search)
+
+    dict['title'] = doc.title
+    dict['when'] = nicedate(datetime.datetime.now() - doc.last_modified)
+    dict['tags'] = [tag.name for tag in doc.tags.all()]
+    dict['text'] = doc.text
     return render_to_response('kb/view.html', dict)
