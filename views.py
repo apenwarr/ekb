f784006780d49396ec1b9047d1595d04874af255
Avery Pennarun <apenwarr@gmail.com>
2009-04-22 23:34:53 -0400

    kb: add a sidebar with the list of available categories.

--- views.py
+++ views.py
@@ -16,8 +16,10 @@ def show(req, search = None):
     dict = {}
     dict['menuitems'] = [('/index/', 'Home'),
 			 ('/kb/', 'Knowledgebase')]
-    dict['page'] = '/kb/'
 
+    dict['alltags'] = Tag.objects.all()
+    dict['alldocs'] = Doc.objects.all()
+    
     doc = _try_get(Doc.objects, id=atoi(search))
     if doc:
 	print req.path
@@ -25,28 +27,33 @@ def show(req, search = None):
 	dict['page'] = page
 	dict['search'] = qsearch
 	if qsearch:
-	    dict['menuitems'].append(('/kb/%s' % qsearch, 'Search "%s"' % qsearch))
+	    dict['menuitems'].append(('/kb/%s' % qsearch, '"%s"' % qsearch))
 	dict['menuitems'].append((page, 'Article #%d' % doc.id))
 	dict['title'] = doc.title
 	dict['when'] = nicedate(datetime.datetime.now() - doc.last_modified)
-	dict['tags'] = [tag.name for tag in doc.tags.all()]
+	dict['tags'] = doc.tags.all()
 	dict['text'] = doc.text
 	return render_to_response('kb/view.html', dict)
     else:
-	if not search: search = ''
+	if not search:
+	    search = ''
 	page = '/kb/%s' % search
 	dict['page'] = page
 	if search:
-	    dict['menuitems'].append((page, 'Search "%s"' % search))
+	    dict['menuitems'].append((page, 'Search'))
 	dict['search'] = search
 	if search:
 	    dict['urlappend'] = '?q=%s' % search
 	dict['title'] = 'Search: "%s"' % search
 
-	f = Doc.objects.all()
-	for word in search.split():
-	    f = f & (Doc.objects.filter(title__icontains = word) |
-		     Doc.objects.filter(text__icontains = word))
+	tag = _try_get(Tag.objects, name__iexact=search)
+	if tag:
+	    f = tag.doc_set.all()
+	else:
+	    f = Doc.objects.all()
+	    for word in search.split():
+		f = f & (Doc.objects.filter(title__icontains = word) |
+			 Doc.objects.filter(text__icontains = word))
 	dict['docs'] = f
 		
 	return render_to_response('kb/search.html', dict)
