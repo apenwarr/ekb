858de2d0c4bba4fb14f8e31c5cfa45f34128ec52
Avery Pennarun <apenwarr@gmail.com>
2009-04-23 16:24:17 -0400

    Maintain a docs/.idmap file to keep kb numbers consistent.
    'make load' now uses the numbers from idmap if they exist, otherwise it adds
    new entries starting at the max current value + 1.

--- load.py
+++ load.py
@@ -3,20 +3,45 @@ from models import Doc, Tag
 
 _fromtimestamp = datetime.datetime.fromtimestamp
 
-def load_all(path):
+def load_all(topdir):
+    seen = {}
+    id_seen = {}
+    name_to_id = {}
+    nextid = 1001
+
+    # The .idmap file lets us maintain consistency between kb article numbers
+    # between loads.  You might not want to check it into version control,
+    # since it'll probably cause merge conflicts.  Only the public-facing
+    # production server needs to maintain the numbers consistently.
+    # (So that google can index things properly.)
+    idfilename = "%s/.idmap" % topdir
+    if os.path.exists(idfilename):
+	for line in open(idfilename):
+	    (id, name) = line.strip().split(" ", 1)
+	    id = int(id)
+	    if id in id_seen:
+		raise KeyError("More than one .idmap entry for #%d" % id)
+	    id_seen[id] = name
+	    name_to_id[name] = id
+	    nextid = max(id+1, nextid)
+    idfile = open(idfilename, "a")
+    
     print 'deleting all'
     Tag.objects.all().delete()
     Doc.objects.all().delete()
 
-    nextid = 1000
-
-    print 'loading all from "%s"' % path
-    for (dirpath, dirnames, filenames) in os.walk(path):
-	assert(dirpath.startswith(path))
-	tags = dirpath[len(path):].split("/")
+    print 'loading all from "%s"' % topdir
+    for (dirpath, dirnames, filenames) in os.walk(topdir):
+	assert(dirpath.startswith(topdir))
+	tags = dirpath[len(topdir):].split("/")
 	for filename in filenames:
 	    if filename[-1] == '~' or filename[0] == '.':
 		continue
+
+	    if filename in seen:
+		raise KeyError('Duplicate filename "%s"' % filename)
+	    seen[filename] = 1
+		
 	    fullpath = os.path.join(dirpath, filename)
 
 	    title = filename
@@ -36,9 +61,14 @@ def load_all(path):
 		line = f.readline()
 
 	    print "  %s (tags=%s)" % (fullpath, repr(tags))
-	    
-	    nextid += 1
-	    d = Doc(id = nextid,
+
+	    id = name_to_id.get(filename)
+	    if not id:
+		id = nextid
+		idfile.write("%d %s\n" % (id, filename))
+	    nextid = max(id+1, nextid)
+		
+	    d = Doc(id = id,
 		    filename = filename,
 		    title = title,
 		    last_modified = _fromtimestamp(os.stat(fullpath)[8]),
