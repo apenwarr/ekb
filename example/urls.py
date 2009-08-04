6544d2d598db6497bb8e908f0fdb40d200c6217f
Avery Pennarun <apenwarr@gmail.com>
2009-08-04 14:54:57 -0400

    Example: change urls.py so reverse URL lookup works.
    For some reason it's important to refer to ekb.views.show by name, not by
    reference.

--- example/urls.py
+++ example/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls.defaults import *
 import os
-import ekb.views
 
 urlpatterns = patterns('',
     # this could be your main company home page; for now, we just redirect to /kb
-    (r'^index$|^index/$|^$', ekb.views.redirect),
+    (r'^index$|^index/$|^$', 
+     'ekb.views.redirect'),
 
     (r'^kb/(?P<id>\d+)(/.+)?\.pdf$',
-     ekb.views.pdf),
+     'ekb.views.pdf'),
     (r'^kb/(?P<search>[^/]*)(/.*)?$',
-     ekb.views.show),
+     'ekb.views.show'),
 
     # WARNING: for testing only, insecure!
     (r'(?P<path>^style/.*)$', 'django.views.static.serve',
